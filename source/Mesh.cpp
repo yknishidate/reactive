07af0bb98e8b340d5a06c86cdb02ce741410a7c9
Yuki Nishidate <asta18425@gmail.com>
2022-05-05 21:15:26 +0900

    add multiple mesh loader

--- source/Mesh.cpp
+++ source/Mesh.cpp
@@ -4,6 +4,35 @@
 
 namespace
 {
+    void LoadShape(const tinyobj::attrib_t& attrib, const tinyobj::shape_t& shape,
+                   std::vector<Vertex>& vertices, std::vector<uint32_t>& indices)
+    {
+        for (const auto& index : shape.mesh.indices) {
+            Vertex vertex{};
+
+            vertex.pos = {
+                attrib.vertices[3 * index.vertex_index + 0],
+               -attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2],
+            };
+
+            vertex.normal = {
+                attrib.normals[3 * index.normal_index + 0],
+               -attrib.normals[3 * index.normal_index + 1],
+                attrib.normals[3 * index.normal_index + 2],
+            };
+
+            vertex.texCoord = {
+                attrib.texcoords[2 * index.texcoord_index + 0],
+                1.0f - attrib.texcoords[2 * index.texcoord_index + 1],
+            };
+
+            vertices.push_back(vertex);
+            indices.push_back(indices.size());
+        }
+    }
+
+    // Load as single mesh
     void LoadFromFile(const std::string& filepath,
                       std::vector<Vertex>& vertices,
                       std::vector<uint32_t>& indices)
@@ -18,29 +47,26 @@ namespace
         }
 
         for (const auto& shape : shapes) {
-            for (const auto& index : shape.mesh.indices) {
-                Vertex vertex{};
+            LoadShape(attrib, shape, vertices, indices);
+        }
+    }
 
-                vertex.pos = {
-                    attrib.vertices[3 * index.vertex_index + 0],
-                   -attrib.vertices[3 * index.vertex_index + 1],
-                    attrib.vertices[3 * index.vertex_index + 2],
-                };
+    // Load as multiple mesh
+    void LoadFromFile(const std::string& filepath,
+                      std::vector<std::vector<Vertex>>& verticesList,
+                      std::vector<std::vector<uint32_t>>& indicesList)
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
 
-                vertex.normal = {
-                    attrib.normals[3 * index.normal_index + 0],
-                   -attrib.normals[3 * index.normal_index + 1],
-                    attrib.normals[3 * index.normal_index + 2],
-                };
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+            throw std::runtime_error(warn + err);
+        }
 
-                vertex.texCoord = {
-                    attrib.texcoords[2 * index.texcoord_index + 0],
-                    1.0f - attrib.texcoords[2 * index.texcoord_index + 1],
-                };
-
-                vertices.push_back(vertex);
-                indices.push_back(indices.size());
-            }
+        for (int meshIndex = 0; meshIndex < shapes.size(); meshIndex++) {
+            LoadShape(attrib, shapes[meshIndex], verticesList[meshIndex], indicesList[meshIndex]);
         }
     }
 }
