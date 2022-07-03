e276e4d80684d6d3201fe21c37706b9b88695ed3
Yuki Nishidate <asta18425@gmail.com>
2022-07-03 13:19:55 +0900

    store all data of resev to image

--- shader/restir/common.glsl
+++ shader/restir/common.glsl
@@ -3,6 +3,7 @@
 #extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
 #extension GL_EXT_buffer_reference2 : require
 #extension GL_EXT_nonuniform_qualifier : enable
+#include "../random.glsl"
 
 precision highp float;
 
@@ -12,6 +13,31 @@ struct HitPayload
 {
     bool shadowed;
 };
+
+struct Reservoir
+{
+    float w_sum; // sum of all weights
+    float p_z;   // probability of target
+    int x_z;     // sample
+    int m;       // number of streaming
+};
+
+bool updateReservoir(inout Reservoir reservoir, int x, float w, float p, inout uint seed)
+{
+    reservoir.w_sum += w;
+    reservoir.m += 1;
+    if (rand(seed) < (w / reservoir.w_sum)) {
+        reservoir.x_z = x;
+        reservoir.p_z = p;
+        return true;
+    }
+    return false;
+}
+
+float calcReservoirWeight(in Reservoir reservoir)
+{
+    return reservoir.w_sum / reservoir.m / reservoir.p_z;
+}
 
 #include "../../share/structure.inc"
 
@@ -24,8 +50,8 @@ layout(binding =  8, rgba16f) uniform image2D normalImage;
 layout(binding = 10, rgba16f) uniform image2D diffuseImage;
 layout(binding = 11, rgba16f) uniform image2D emissionImage;
 layout(binding =  9, rgba8ui) uniform uimage2D instanceIndexImage;
-layout(binding =  12, rgba8ui) uniform uimage2D resevSampleImage;
-layout(binding =  13, rgba16f) uniform image2D resevWeightImage;
+layout(binding =  12, rgba8ui) uniform uimage2D resevSampleImage; // (x_z, m)
+layout(binding =  13, rgba16f) uniform image2D resevWeightImage;  // (w_sum, p_z)
 layout(binding =  3) buffer Addresses { BufferAddress address[]; } addresses;
 layout(binding =  4) uniform sampler2D samplers[];
 
