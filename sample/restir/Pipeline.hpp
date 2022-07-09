c32227ee47508f7d1cb9b9e587aef15d1df24645
Yuki Nishidate <asta18425@gmail.com>
2022-07-09 22:50:47 +0900

    delete pipeline.cpp

--- sample/restir/Pipeline.hpp
+++ sample/restir/Pipeline.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vulkan/Pipeline.hpp"
+#include "Window/Window.hpp"
 
 struct GBuffers
 {
@@ -13,7 +14,27 @@ struct GBuffers
 class GBufferPipeline : public RayTracingPipeline
 {
 public:
-    GBufferPipeline(const Scene& scene, size_t pushSize = 0);
+    GBufferPipeline(const Scene& scene, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "gbuffer/gbuffer.rgen",
+                                        SHADER_DIR + "gbuffer/gbuffer.rmiss",
+                                        SHADER_DIR + "gbuffer/gbuffer.rchit");
+        RegisterScene(scene);
+
+        gbuffers.position = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16B16A16Sfloat };
+        gbuffers.normal = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16B16A16Sfloat };
+        gbuffers.diffuse = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eB8G8R8A8Unorm };
+        gbuffers.emission = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16B16A16Sfloat };
+        gbuffers.instanceIndex = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR8G8B8A8Uint };
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     const GBuffers& GetGBuffers() const { return gbuffers; }
 
@@ -24,7 +45,24 @@ private:
 class UniformLightPipeline : public RayTracingPipeline
 {
 public:
-    UniformLightPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0);
+    UniformLightPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "uniform_light/uniform_light.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+
+        outputImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eB8G8R8A8Unorm };
+        Register("outputImage", outputImage);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     const Image& GetOutputImage() const { return outputImage; }
 
@@ -35,7 +73,24 @@ private:
 class WRSPipeline : public RayTracingPipeline
 {
 public:
-    WRSPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0);
+    WRSPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "wrs/wrs.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+
+        outputImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eB8G8R8A8Unorm };
+        Register("outputImage", outputImage);
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     const Image& GetOutputImage() const { return outputImage; }
 
@@ -52,7 +107,27 @@ struct ResevImages
 class InitResevPipeline : public RayTracingPipeline
 {
 public:
-    InitResevPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0);
+    InitResevPipeline(const Scene& scene, const GBuffers& gbuffers, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "restir/init_resev.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+
+        resevImages.sampleImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR32G32Uint };
+        resevImages.weightImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16Sfloat };
+        Register("resevSampleImage", resevImages.sampleImage);
+        Register("resevWeightImage", resevImages.weightImage);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     ResevImages& GetResevImages() { return resevImages; }
 
@@ -63,11 +138,47 @@ private:
 class SpatialReusePipeline : public RayTracingPipeline
 {
 public:
-    SpatialReusePipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0);
+    SpatialReusePipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "restir/spatial_reuse.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+        Register("resevSampleImage", resevImages.sampleImage);
+        Register("resevWeightImage", resevImages.weightImage);
+
+        newResevImages.sampleImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR32G32Uint };
+        newResevImages.weightImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16Sfloat };
+        Register("newResevSampleImage", newResevImages.sampleImage);
+        Register("newResevWeightImage", newResevImages.weightImage);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     ResevImages& GetNewResevImages() { return newResevImages; }
 
-    void CopyToResevImages(ResevImages& dst);
+    void CopyToResevImages(ResevImages& dst)
+    {
+        newResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        newResevImages.weightImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        dst.sampleImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+        dst.weightImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+
+        newResevImages.sampleImage.CopyToImage(dst.sampleImage);
+        newResevImages.weightImage.CopyToImage(dst.weightImage);
+
+        newResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        newResevImages.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        dst.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        dst.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+    }
 
 private:
     ResevImages newResevImages;
@@ -76,12 +187,67 @@ private:
 class TemporalReusePipeline : public RayTracingPipeline
 {
 public:
-    TemporalReusePipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0);
+    TemporalReusePipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "restir/temporal_reuse.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+        Register("resevSampleImage", resevImages.sampleImage);
+        Register("resevWeightImage", resevImages.weightImage);
+
+        newResevImages.sampleImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR32G32Uint };
+        newResevImages.weightImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16Sfloat };
+        oldResevImages.sampleImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR32G32Uint };
+        oldResevImages.weightImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eR16G16Sfloat };
+        Register("newResevSampleImage", newResevImages.sampleImage);
+        Register("newResevWeightImage", newResevImages.weightImage);
+        Register("oldResevSampleImage", oldResevImages.sampleImage);
+        Register("oldResevWeightImage", oldResevImages.weightImage);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     ResevImages& GetNewResevImages() { return newResevImages; }
 
-    void CopyToResevImages(ResevImages& dst);
-    void CopyFromResevImages(ResevImages& src);
+    void CopyToResevImages(ResevImages& dst)
+    {
+        newResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        newResevImages.weightImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        dst.sampleImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+        dst.weightImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+
+        newResevImages.sampleImage.CopyToImage(dst.sampleImage);
+        newResevImages.weightImage.CopyToImage(dst.weightImage);
+
+        newResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        newResevImages.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        dst.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        dst.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+    }
+
+    void CopyFromResevImages(ResevImages& src)
+    {
+        src.sampleImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        src.weightImage.SetImageLayout(vk::ImageLayout::eTransferSrcOptimal);
+        oldResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+        oldResevImages.weightImage.SetImageLayout(vk::ImageLayout::eTransferDstOptimal);
+
+        src.sampleImage.CopyToImage(oldResevImages.sampleImage);
+        src.weightImage.CopyToImage(oldResevImages.weightImage);
+
+        src.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        src.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        oldResevImages.sampleImage.SetImageLayout(vk::ImageLayout::eGeneral);
+        oldResevImages.weightImage.SetImageLayout(vk::ImageLayout::eGeneral);
+    }
 
 private:
     ResevImages newResevImages;
@@ -91,7 +257,27 @@ private:
 class ShadingPipeline : public RayTracingPipeline
 {
 public:
-    ShadingPipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0);
+    ShadingPipeline(const Scene& scene, const GBuffers& gbuffers, const ResevImages& resevImages, size_t pushSize = 0)
+    {
+        RayTracingPipeline::LoadShaders(SHADER_DIR + "restir/shading.rgen",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rmiss",
+                                        SHADER_DIR + "shadow_ray/shadow_ray.rchit");
+
+        RegisterScene(scene);
+
+        Register("positionImage", gbuffers.position);
+        Register("normalImage", gbuffers.normal);
+        Register("diffuseImage", gbuffers.diffuse);
+        Register("emissionImage", gbuffers.emission);
+        Register("instanceIndexImage", gbuffers.instanceIndex);
+        Register("resevSampleImage", resevImages.sampleImage);
+        Register("resevWeightImage", resevImages.weightImage);
+
+        outputImage = Image{ Window::GetWidth(), Window::GetHeight(), vk::Format::eB8G8R8A8Unorm };
+        Register("outputImage", outputImage);
+
+        RayTracingPipeline::Setup(pushSize);
+    }
 
     const Image& GetOutputImage() const { return outputImage; }
 
