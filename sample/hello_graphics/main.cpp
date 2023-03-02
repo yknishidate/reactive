bb4605f554fffec13a2f45cb80bd783a6299b0d0
nishidate-yuki <yuki18425@gmail.com>
2023-03-02 18:38:16 +0900

    update HelloGraphics

--- sample/hello_graphics/main.cpp
+++ sample/hello_graphics/main.cpp
@@ -12,8 +12,6 @@ std::string vertCode = R"(
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inNormal;
 layout(location = 2) in vec2 inTexCoord;
-layout(location = 0) out vec3 fragColor;
-layout(location = 1) out vec3 fragNormal;
 layout(push_constant) uniform PushConstants {
     mat4 model;
     mat4 view;
@@ -22,35 +20,30 @@ layout(push_constant) uniform PushConstants {
 
 void main() {
     gl_Position = pushConstants.proj * pushConstants.view * pushConstants.model * vec4(inPosition, 1.0);
-    fragColor = vec3(1);
-    fragNormal = inNormal;
 })";
 
 std::string fragCode = R"(
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
-layout(location = 0) in vec3 fragColor;
-layout(location = 1) in vec3 fragNormal;
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(fragColor, 1.0) * dot(fragNormal, vec3(1.0));
+    outColor = vec4(1.0);
 })";
 
 int main() {
     try {
         Log::init();
         Window::init(750, 750);
-        Context::init();
+        Context::init(true);
 
         Swapchain swapchain{};
         GUI gui{swapchain};
 
-        // std::vector<Vertex> vertices{ {{-1, 0, 0}}, {{ 0, -1, 0}}, {{ 1, 0, 0}} };
-        // std::vector<Index> indices{ 0, 1, 2 };
-        // Mesh mesh{ vertices, indices };
+        std::vector<Vertex> vertices{{{-1, 0, 0}}, {{0, -1, 0}}, {{1, 0, 0}}};
+        std::vector<Index> indices{0, 1, 2};
+        Mesh mesh{vertices, indices, 0};
 
-        Mesh mesh{"bunny.obj"};
         Camera camera{Window::getWidth(), Window::getHeight()};
 
         Shader vertShader{vertCode, vk::ShaderStageFlagBits::eVertex};
@@ -61,10 +54,12 @@ int main() {
         descSet.addResources(fragShader);
         descSet.allocate();
 
-        GraphicsPipeline pipeline{descSet};
-        pipeline.setVertexShader(vertShader);
-        pipeline.setFragmentShader(fragShader);
-        pipeline.setup(swapchain, sizeof(PushConstants));
+        GraphicsPipeline pipeline;
+        pipeline.setDescriptorSet(descSet);
+        pipeline.addShader(vertShader);
+        pipeline.addShader(fragShader);
+        pipeline.setPushSize(sizeof(PushConstants));
+        pipeline.setup(swapchain.getRenderPass());
 
         int testInt = 0;
         int frame = 0;
@@ -86,10 +81,10 @@ int main() {
             commandBuffer.bindPipeline(pipeline);
             commandBuffer.pushConstants(pipeline, &pushConstants);
             commandBuffer.clearBackImage({0.0f, 0.0f, 0.3f, 1.0f});
-            commandBuffer.beginRenderPass();
+            commandBuffer.beginDefaultRenderPass();
             commandBuffer.drawIndexed(mesh);
             commandBuffer.drawGUI(gui);
-            commandBuffer.endRenderPass();
+            commandBuffer.endDefaultRenderPass();
             commandBuffer.submit();
 
             swapchain.present();
