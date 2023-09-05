c582c3fc9fb54b320e02664ebc9c29a68b75308a
nishidate-yuki <yuki18425@gmail.com>
2023-09-05 18:31:20 +0900

    fix perf warning

--- sample/editor/main.cpp
+++ sample/editor/main.cpp
@@ -47,6 +47,8 @@ public:
         const std::string gridVertCode = R"(
         #version 450
         layout(location = 0) in vec3 inPosition;
+        layout(location = 1) in vec3 inNormal;
+        layout(location = 2) in vec2 inTexCoord;
         layout(push_constant) uniform PushConstants {
             mat4 viewProj;
             vec3 color;
