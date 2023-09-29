ea3b772c85d51b1ca6fdc89a66e221f58b5f4d07
nishidate-yuki <yuki18425@gmail.com>
2023-09-29 22:17:56 +0900

    remove colors from assert message

--- include/reactive/common.hpp
+++ include/reactive/common.hpp
@@ -6,16 +6,11 @@ namespace rv {
 #ifdef NDEBUG
 #define RV_ASSERT(condition, ...)
 #else
-#define RV_ASSERT(condition, ...)                                                           \
-    constexpr char RED_COLOR[] = "\033[31m";                                                \
-    constexpr char YELLOW_COLOR[] = "\033[33m";                                             \
-    constexpr char RESET_COLOR[] = "\033[0m";                                               \
-    if (!(condition)) {                                                                     \
-        spdlog::critical("Assertion {}`" #condition                                         \
-                         "`{} failed. {}{}{}\n           File: {}\n           Line: {}",    \
-                         YELLOW_COLOR, RESET_COLOR, YELLOW_COLOR, fmt::format(__VA_ARGS__), \
-                         RESET_COLOR, __FILE__, __LINE__);                                  \
-        std::terminate();                                                                   \
+#define RV_ASSERT(condition, ...)                                                            \
+    if (!(condition)) {                                                                      \
+        spdlog::critical("Assertion `" #condition "` failed.\n\t{}\n\tFile: {}\n\tLine: {}", \
+                         fmt::format(__VA_ARGS__), __FILE__, __LINE__);                      \
+        std::terminate();                                                                    \
     }
 #endif
 }  // namespace rv
