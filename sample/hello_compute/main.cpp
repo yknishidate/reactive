e82ab8a2e7aed65963e91acdd6b23438df989dbc
nishidate-yuki <yuki18425@gmail.com>
2023-10-02 01:05:49 +0900

    draw mandelbrot

--- sample/hello_compute/main.cpp
+++ sample/hello_compute/main.cpp
@@ -2,6 +2,12 @@
 
 using namespace rv;
 
+struct MandelbrotParams {
+    glm::vec2 lowerLeft = {-1, -1};
+    glm::vec2 upperRight = {1, 1};
+    int maxIterations = 10;
+};
+
 class HelloApp : public App {
 public:
     HelloApp()
@@ -19,7 +25,15 @@ public:
             .format = vk::Format::eB8G8R8A8Unorm,
         });
 
+        buffer = context.createBuffer({
+            .usage = BufferUsage::Uniform,
+            .memory = MemoryUsage::Device,
+            .size = sizeof(MandelbrotParams),
+            .debugName = "buffer",
+        });
+
         context.oneTimeSubmit([&](CommandBufferHandle commandBuffer) {
+            commandBuffer->copyBuffer(buffer, &params);
             commandBuffer->transitionLayout(image, vk::ImageLayout::eGeneral);
         });
 
@@ -30,33 +44,53 @@ public:
 
         descSet = context.createDescriptorSet({
             .shaders = compShader,
+            .buffers = {{"MandelbrotParams", buffer}},
             .images = {{"outputImage", image}},
         });
 
         pipeline = context.createComputePipeline({
             .computeShader = compShader,
             .descSetLayout = descSet->getLayout(),
-            .pushSize = sizeof(int),
         });
     }
 
-    void onUpdate() override { frame++; }
+    void onScroll(double xoffset, double yoffset) override {
+        spdlog::info("scroll: {}", yoffset);
+        scale *= 1.0f + yoffset * 0.1f;
+    }
+
+    void onCursorPos(double xpos, double ypos) override {
+        static glm::vec2 lastCursorPos{0.0f};
+        glm::vec2 cursorPos = {xpos, ypos};
+        glm::vec2 cursorOffset = cursorPos - lastCursorPos;
+        lastCursorPos = cursorPos;
+        if (isMouseButtonDown(GLFW_MOUSE_BUTTON_1)) {
+            spdlog::info("cursor: {}", glm::to_string(cursorOffset));
+            translate += -cursorOffset / static_cast<float>(height) / scale;
+        }
+    }
 
     void onRender(const CommandBufferHandle& commandBuffer) override {
-        ImGui::SliderInt("Test slider", &testInt, 0, 100);
+        float aspect = width / static_cast<float>(height);
+        params.lowerLeft = glm::vec2(-1 * aspect, -1) / scale + translate;
+        params.upperRight = glm::vec2(1 * aspect, 1) / scale + translate;
+        params.maxIterations = scale * 10;
+
+        commandBuffer->copyBuffer(buffer, &params);
         commandBuffer->bindDescriptorSet(descSet, pipeline);
         commandBuffer->bindPipeline(pipeline);
-        commandBuffer->pushConstants(pipeline, &frame);
         commandBuffer->dispatch(width, height, 1);
         commandBuffer->copyImage(image, getCurrentColorImage(), vk::ImageLayout::eGeneral,
                                  vk::ImageLayout::ePresentSrcKHR);
     }
 
+    float scale = 1.0f;
+    glm::vec2 translate = {0.0f, 0.0f};
+    MandelbrotParams params;
     ImageHandle image;
+    BufferHandle buffer;
     DescriptorSetHandle descSet;
     ComputePipelineHandle pipeline;
-    int testInt = 0;
-    int frame = 0;
 };
 
 int main() {
