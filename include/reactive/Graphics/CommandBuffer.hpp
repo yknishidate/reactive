95806f9c0b731287921a11686a8dec3cdf6ded86
nishidate-yuki <yuki18425@gmail.com>
2023-10-01 17:57:52 +0900

    move impl

--- include/reactive/Graphics/CommandBuffer.hpp
+++ include/reactive/Graphics/CommandBuffer.hpp
@@ -154,31 +154,7 @@ public:
 
     void buildTopAccel(TopAccelHandle topAccel) const;
 
-    void buildBottomAccel(BottomAccelHandle bottomAccel) const {
-        vk::AccelerationStructureGeometryDataKHR geometryData;
-        geometryData.setTriangles(bottomAccel->trianglesData);
-
-        vk::AccelerationStructureGeometryKHR geometry;
-        geometry.setGeometryType(vk::GeometryTypeKHR::eTriangles);
-        geometry.setGeometry({geometryData});
-        geometry.setFlags(bottomAccel->geometryFlags);
-
-        vk::AccelerationStructureBuildGeometryInfoKHR buildGeometryInfo;
-        buildGeometryInfo.setType(vk::AccelerationStructureTypeKHR::eBottomLevel);
-        buildGeometryInfo.setFlags(bottomAccel->buildFlags);
-        buildGeometryInfo.setGeometries(geometry);
-
-        buildGeometryInfo.setMode(vk::BuildAccelerationStructureModeKHR::eBuild);
-        buildGeometryInfo.setDstAccelerationStructure(*bottomAccel->accel);
-        buildGeometryInfo.setScratchData(bottomAccel->scratchBuffer->getAddress());
-
-        vk::AccelerationStructureBuildRangeInfoKHR buildRangeInfo{};
-        buildRangeInfo.setPrimitiveCount(bottomAccel->primitiveCount);
-        buildRangeInfo.setPrimitiveOffset(0);
-        buildRangeInfo.setFirstVertex(0);
-        buildRangeInfo.setTransformOffset(0);
-        commandBuffer->buildAccelerationStructuresKHR(buildGeometryInfo, &buildRangeInfo);
-    }
+    void buildBottomAccel(BottomAccelHandle bottomAccel) const;
 
     // timestamp
     void beginTimestamp(GPUTimerHandle gpuTimer) const;
