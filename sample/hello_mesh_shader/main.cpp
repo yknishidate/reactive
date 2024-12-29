13ceedcfd8d9ac17b8ade335a9c76c2e263419cb
Yuki Nishidate <yuki18425@gmail.com>
2024-12-29 12:03:08 +0900

    MeshShaderサンプルをSlangに移行

--- sample/hello_mesh_shader/main.cpp
+++ sample/hello_mesh_shader/main.cpp
@@ -2,72 +2,31 @@
 
 using namespace rv;
 
-std::string meshCode = R"(
-#version 450
-#extension GL_EXT_mesh_shader : require
-
-layout(local_size_x = 4, local_size_y = 1, local_size_z = 1) in;
-layout(triangles, max_vertices = 4, max_primitives = 2) out;
-
-layout(location = 0) out VertexOutput
-{
-    vec4 color;
-} vertexOutput[];
-
-vec2 positions[] = vec2[](
-    vec2(-0.5, -0.5),
-    vec2( 0.5, -0.5),
-    vec2( 0.5,  0.5),
-    vec2(-0.5,  0.5)
-);
-
-uvec3 indices[] = uvec3[](
-    uvec3(0, 1, 2),
-    uvec3(2, 3, 0)
-);
-
-void main()
-{
-    uint gid = gl_GlobalInvocationID.x;
-    SetMeshOutputsEXT(4, 2);
-    gl_MeshVerticesEXT[gid].gl_Position.xy = positions[gid];
-    vertexOutput[gid].color.xy = positions[gid] + 0.5;
-    if(gid < 2){
-        gl_PrimitiveTriangleIndicesEXT[gid] = indices[gid];
-    }
-})";
-
-std::string fragCode = R"(
-#version 450
-layout (location = 0) in VertexInput {
-    vec4 color;
-};
-layout(location = 0) out vec4 outColor;
-
-void main() {
-    outColor = color;
-})";
-
 class HelloApp : public App {
 public:
     HelloApp()
         : App({
               .width = 1280,
               .height = 720,
-              .title = "HelloGraphics",
+              .title = "HelloMeshShader",
               .layers = {Layer::Validation},
               .extensions = {Extension::MeshShader},
           }) {}
 
     void onStart() override {
+        SlangCompiler compiler;
+        auto codes = compiler.CompileShaders(SHADER_PATH, {"meshMain", "fragmentMain"});
+
         std::vector<ShaderHandle> shaders(2);
         shaders[0] = context.createShader({
-            .code = Compiler::compileToSPV(meshCode, vk::ShaderStageFlagBits::eMeshEXT),
+            .pCode = codes[0]->getBufferPointer(),
+            .codeSize = codes[0]->getBufferSize(),
             .stage = vk::ShaderStageFlagBits::eMeshEXT,
         });
 
         shaders[1] = context.createShader({
-            .code = Compiler::compileToSPV(fragCode, vk::ShaderStageFlagBits::eFragment),
+            .pCode = codes[1]->getBufferPointer(),
+            .codeSize = codes[1]->getBufferSize(),
             .stage = vk::ShaderStageFlagBits::eFragment,
         });
 
